103ac97497ad3e756684beb2fea46964a72048d2
Taras Oleksiuk <syskonsult007>
2017-02-09 18:01:21 +0200

    Add menu items

--- app/src/main/java/com/example/remindme/MainActivity.java
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.remindme;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,14 +8,18 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -27,5 +32,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 }
